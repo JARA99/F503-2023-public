8d91d18181260a907462dd0fe832e9c18eea42b5
Jorge Alejandro <41133772+JARA99@users.noreply.github.com>
2024-02-23 08:29:12 -0600

    Create test_github_edit.py

---
+++ resources/test_github_edit.py
@@ -0,0 +1,11 @@
+# file: test_github_edit.py
+
+import streamlit as st
+import pandas as pd
+
+st.title('Titulo 1')
+
+data = pd.read_csv('https://raw.githubusercontent.com/mwaskom/seaborn-data/master/penguins.csv')
+
+st.table(data)
+
