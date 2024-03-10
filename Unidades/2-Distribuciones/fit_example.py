6b5a5a811988b8e34233ac0fd9a2ee75ffb3055f
Jorge Alejandro <jorgealejandro1999@gmail.com>
2024-03-10 00:10:39 -0600

    adding fit_example2

--- Unidades/2-Distribuciones/fit_example.py
+++ Unidades/2-Distribuciones/fit_example.py
@@ -33,7 +33,7 @@ print(f'data:\n{data}')
 
 data = data.loc[:m]
 
-counts_non_sort = data['GM'].value_counts()
+counts_non_sort = data['GS'].value_counts()
 counts = pd.DataFrame(np.zeros(11))
 # print(counts)
 
---
+++ Unidades/2-Distribuciones/fit_example2.py
@@ -0,0 +1,17 @@
+# file: fit.py
+
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from scipy import stats as ss
+
+m = 100
+
+data = pd.read_csv('Binomial-fichas.csv')
+data = data.loc[:m,'GM']
+print(f'data:\n{data}')
+
+fitted_results = ss.fit(ss.binom,data,bounds=[(0,100),(0,1)])
+print(fitted_results)
+fitted_results.plot()
+plt.show()
